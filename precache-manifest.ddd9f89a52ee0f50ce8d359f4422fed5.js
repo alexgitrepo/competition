dd4f1868f06954b6e993adbd8ca9c8e9fd858dea
alexgitrepo <alex.dev4web.com>
2020-03-15 18:48:26 +0500

    Updates

---
+++ precache-manifest.ddd9f89a52ee0f50ce8d359f4422fed5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facf6619769d0bf1ce08a6658d94782c",
+    "url": "/competition/index.html"
+  },
+  {
+    "revision": "a78ee2d333e2da07da50",
+    "url": "/competition/static/css/main.e3632a6c.chunk.css"
+  },
+  {
+    "revision": "b64fc6c66a98bbca8afb",
+    "url": "/competition/static/js/2.b46f7bf3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/competition/static/js/2.b46f7bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78ee2d333e2da07da50",
+    "url": "/competition/static/js/main.7709c3a0.chunk.js"
+  },
+  {
+    "revision": "e5420910ee1565c3c701",
+    "url": "/competition/static/js/runtime-main.11a63cc3.js"
+  },
+  {
+    "revision": "bba397dbd225ce41348ecd3263a5f6e6",
+    "url": "/competition/static/media/down-arrow.bba397db.svg"
+  },
+  {
+    "revision": "13e1a3addf5c0fb7b3d317e3b29a2731",
+    "url": "/competition/static/media/up-arrow.13e1a3ad.svg"
+  },
+  {
+    "revision": "673d7674434446e5932dcdf42a0ff9f3",
+    "url": "/competition/static/media/warning-speech-bubble.673d7674.svg"
+  }
+]);
